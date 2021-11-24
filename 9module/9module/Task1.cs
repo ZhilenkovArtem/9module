6bc95925b7820b8aa9861f81d3ad27175169118c
Zhilenkov Artem <zhilenkovartem@mail.ru>
2021-11-24 21:10:18 +0300

    1 task

---
+++ 9module/9module/Task1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _9module
+{
+    class Task1
+    {
+        static void Main()
+        {
+            Exception[] exceptions = new Exception[]
+            {
+                new NewException(),
+                new ArgumentException(),
+                new ArgumentNullException(),
+                new ExecutionEngineException(),
+                new FieldAccessException()
+            };
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                foreach (var exception in exceptions)
+                {
+                    if (ex.GetType() == exception.GetType())
+                        Console.WriteLine(ex.GetType());
+                }
+            }
+        }
+    }
+
+    class NewException : Exception
+    { }
+}
